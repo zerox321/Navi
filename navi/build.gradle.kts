11e38ecca114da3d49dc5019217fee4481411eb9
Eslam Kamel <eslamesw@gmail.com>
2024-09-08 15:08:20 +0300

    update lib version

--- navi/build.gradle.kts
+++ navi/build.gradle.kts
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.zerox"
                 artifactId = "navi"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
         repositories {
